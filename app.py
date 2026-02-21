6fb5b398ccf6cbc19f5a91b37e5fa6fb09b4bc05
CacaoVainillaClub <qmonica326@gmail.com>
2026-02-21 20:16:25 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Club Cacao & Vainilla", page_icon="🍦")
+st.title("🍦 Club Cacao & Vainilla")
+st.subheader("¡Bienvenido a tu sistema de puntos!")
+st.write("¡Lo lograste! Tu app ya está en internet.")
